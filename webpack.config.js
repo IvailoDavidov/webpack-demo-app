259f19193036e6b0d8140f8a1faaa41c0e1099af
Ivaylo Davidov <ivaylodavidov@idavidov-mac.local>
2023-10-11 15:00:20 +0300

    add first loader translating css to JavaScript

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ entry: "./src/index.js",
 output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+ },
+module: {
+    rules: [
+        {
+        test: /\.css$/,
+        use: ["style-loader","css-loader"]
+        }
+    ]
  }
 };
