07fc46b6dd93d11a04ec5aa947116c9ed2c11175
Ivaylo Davidov <ivaylodavidov@idavidov-mac.local>
2023-10-12 11:27:47 +0300

    Add prod and dev configs, merge  and install dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-mode: "development",
-entry: "./src/index.js",
-output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist")
- },
- plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
- })
-],
-module: {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
     rules: [
-        {
-          test: /\.scss$/,
-          use: [
-          "style-loader",  //3  Inject styles into DOM    
-          "css-loader",    //2  Turns CSS into common JS
-          "sass-loader"   //1  Turns SASS into CSS
-          ]    
-        }
-    ]
- }
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3  Inject styles into DOM
+          "css-loader", //2  Turns CSS into common JS
+          "sass-loader", //1  Turns SASS into CSS
+        ],
+      },
+    ],
+  },
 };
